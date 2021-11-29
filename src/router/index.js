669d87933caf215cbab0556c38c4ba4b798cfcd5
Paweł Płonka <pjplonka@gmail.com>
2021-11-29 15:19:37 +0100

    Remove initial files and prepare for more applications.

--- src/router/index.js
+++ src/router/index.js
@@ -1,31 +1,32 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import store from '@/store/index'
+import adminRoutes from "@/applications/Admin/routes";
+import publicRoutes from "@/applications/Public/routes";
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/About.vue')
-    }
-  }
+  adminRoutes,
+  publicRoutes,
 ]
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (!store.getters.isAuthenticated) {
+      next("/login");
+    } else {
+      next()
+    }
+  } else {
+    next()
+  }
 })
 
 export default router
